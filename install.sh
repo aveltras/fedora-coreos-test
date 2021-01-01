f787d93e394e4ad51ebe626b8af54689d45fbba7
aveltras <romain.viallard@outlook.fr>
2021-01-01 16:20:58 +0100

    Update

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo docker run --pull=always --privileged --rm \
+    -v /dev:/dev -v /run/udev:/run/udev -v .:/data -w /data \
+    quay.io/coreos/coreos-installer:release \
+    install /dev/sda -i config.ign
